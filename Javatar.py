9802aa6c4c1485e535eb2082b2896f10d52e1fde
Sirisak Luengsaksri <spywhere@me.com>
2013-07-15 03:44:55 +0700

    Initial Commit

---
+++ Javatar.py
@@ -0,0 +1,51 @@
+import sublime_plugin
+
+
+class JavatarCallCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Package")
+
+
+class PackageNameCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Package")
+
+
+class FullPackageNameCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Package")
+
+
+class CreateSubpackageCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Subpackage")
+
+
+class CreateClassCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Subpackage")
+
+
+class CreateInterfaceCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Subpackage")
+
+
+class CreateEnumCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Subpackage")
+
+
+class BuildProjectCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Subpackage")
+
+
+class CreateJarFileCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Subpackage")
+
+
+class CreateExeJarFileCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Create Subpackage")
