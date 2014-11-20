a9cf5e2dc0b22430b705987d8724e4de06b8ef4f
Dominic May <jack.thatch@gmail.com>
2014-11-20 12:37:38 +0800

    Split out import organization steps

--- commands/javatar_operation.py
+++ commands/javatar_operation.py
@@ -100,187 +100,214 @@ class JavatarOrganizeImportsCommand(sublime_plugin.TextCommand):
                     classes.append(textScope)
         return classes
 
-    def run(self, edit, step=0):
-        if step == 0:
-            #gathering info
-            self.reset()
-            add_action("javatar.command.operation.organize_imports.step0", "Organize Imports [step=0] Gathering info")
-            importedPackagesRegions = self.view.find_by_selector(get_settings("package_import_selector"))
-            useTypesRegions = self.view.find_by_selector(get_settings("type_selector"))
+    def organize_step_zero(self, edit):
+        # gathering info
+        self.reset()
+        add_action("javatar.command.operation.organize_imports.step0", "Organize Imports [step=0] Gathering info")
+        importedPackagesRegions = self.view.find_by_selector(get_settings("package_import_selector"))
+        useTypesRegions = self.view.find_by_selector(get_settings("type_selector"))
 
-            primitiveTypes = get_settings("primitive_type")
+        primitiveTypes = get_settings("primitive_type")
 
-            for region in useTypesRegions:
-                if self.view.substr(region) not in primitiveTypes:
-                    self.useTypes += self.getClasses(self.view.substr(region))
-                elif is_debug():
-                    print("useType: " + self.view.substr(region))
+        for region in useTypesRegions:
+            if self.view.substr(region) not in primitiveTypes:
+                self.useTypes += self.getClasses(self.view.substr(region))
+            elif is_debug():
+                print("useType: " + self.view.substr(region))
 
-            for region in importedPackagesRegions:
-                package = self.view.substr(region)
-                self.importedPackages.append(package)
-                self.importedTypes.append(get_class_name(self.view.substr(region)))
-                if get_package_path(package) in self.importedPackagesStat:
-                    self.importedPackagesStat[get_package_path(package)] += 1
-                else:
-                    self.importedPackagesStat[get_package_path(package)] = 1
-
-            for useType in self.useTypes:
-                if useType not in self.importedTypes and useType not in self.needImportTypes and (not is_file() or (is_file() and not get_path("exist", get_path("join", get_path("current_dir"), useType + ".java")))):
-                    self.needImportTypes.append(useType)
-
-            self.index = 0
-            self.run(edit, 1)
-        elif step == 1:
-            #select classes
-            add_action("javatar.command.operation.organize_imports.step1", "Organize Imports [step=1] Select classes")
-            if len(self.needImportTypes) > 0 and self.index < len(self.needImportTypes):
-                classes = find_class(get_package_root_dir(), self.needImportTypes[self.index])
-                if len(classes) > 0:
-                    self.selectClasses(None, classes)
-                else:
-                    if self.needImportTypes[self.index] not in self.askTypes:
-                        self.askTypes.append(self.needImportTypes[self.index])
-                    self.index += 1
-                    self.run(edit, 1)
+        for region in importedPackagesRegions:
+            package = self.view.substr(region)
+            self.importedPackages.append(package)
+            self.importedTypes.append(get_class_name(self.view.substr(region)))
+            if get_package_path(package) in self.importedPackagesStat:
+                self.importedPackagesStat[get_package_path(package)] += 1
             else:
-                self.index = 0
-                self.run(edit, 3)
-        elif step == 2:
-            # select classes callback
-            add_action(
-                "javatar.command.operation.organize_imports.step2",
-                "Organize Imports [step=2] Select classes callback"
-            )
-            if isinstance(self.selectedPackage, int):
-                if self.needImportTypes[self.index] not in self.postAskTypes:
-                    self.postAskTypes.append(self.needImportTypes[self.index])
+                self.importedPackagesStat[get_package_path(package)] = 1
+
+        for useType in self.useTypes:
+            if useType not in self.importedTypes and useType not in self.needImportTypes and (not is_file() or (is_file() and not get_path("exist", get_path("join", get_path("current_dir"), useType + ".java")))):
+                self.needImportTypes.append(useType)
+
+        self.index = 0
+        self.run(edit, 1)
+
+    def organize_step_one(self, edit):
+        # select classes
+        add_action("javatar.command.operation.organize_imports.step1", "Organize Imports [step=1] Select classes")
+        if len(self.needImportTypes) > 0 and self.index < len(self.needImportTypes):
+            classes = find_class(get_package_root_dir(), self.needImportTypes[self.index])
+            if len(classes) > 0:
+                self.selectClasses(None, classes)
             else:
-                if self.selectedPackage is not None:
-                    if get_package_path(self.selectedPackage) in self.importedPackagesStat:
-                        self.importedPackagesStat[get_package_path(self.selectedPackage)] += 1
-                    else:
-                        self.importedPackagesStat[get_package_path(self.selectedPackage)] = 1
-                    self.importedPackages.append(self.selectedPackage)
-                    self.importedTypes.append(self.needImportTypes[self.index])
-            self.index += 1
-            if self.index >= len(self.needImportTypes):
-                self.index = 0
-                self.run(edit, 3)
-            else:
+                if self.needImportTypes[self.index] not in self.askTypes:
+                    self.askTypes.append(self.needImportTypes[self.index])
+                self.index += 1
                 self.run(edit, 1)
-        elif step == 3:
-            # add default imports
-            add_action(
-                "javatar.command.operation.organize_imports.step3",
-                "Organize Imports [step=3] Add default imports"
-            )
-            for packageImport in get_packages():
-                importOnce = False
-                if "packages" in packageImport:
-                    for packageName in packageImport["packages"]:
-                        package = packageImport["packages"][packageName]
-                        for importType in get_all_types(package):
-                            if importType in self.askTypes:
-                                importOnce = True
-                                self.askTypes.remove(importType)
-                                if "default" in package and package["default"]:
-                                    continue
-                                packageCode = packageName + "." + importType
-                                if packageCode not in self.importedPackages:
-                                    self.importedPackages.append(packageCode)
-                                if packageCode in self.importedPackagesStat:
-                                    self.importedPackagesStat[packageCode] += 1
-                                else:
-                                    self.importedPackagesStat[packageCode] = 1
-                        if not importOnce and "always_import" in packageImport and packageImport["always_import"]:
-                            self.alwaysImportedPackages.append(packageName)
-            self.run(edit, 4)
-        elif step == 4:
-            # ask package
-            add_action(
-                "javatar.command.operation.organize_imports.step4",
-                "Organize Imports [step=4] Ask package"
-            )
-            self.askTypes += self.postAskTypes
-            if len(self.askTypes) > 0 and self.index < len(self.askTypes):
-                self.askPackage(-1, self.askTypes[self.index])
-            else:
-                self.run(edit, 6)
-        elif step == 5:
-            # ask package callback
-            add_action(
-                "javatar.command.operation.organize_imports.step5",
-                "Organize Imports [step=5] Ask package callback"
-            )
+        else:
+            self.index = 0
+            self.run(edit, 3)
+
+    def organize_step_two(self, edit):
+        # select classes callback
+        add_action(
+            "javatar.command.operation.organize_imports.step2",
+            "Organize Imports [step=2] Select classes callback"
+        )
+        if isinstance(self.selectedPackage, int):
+            if self.needImportTypes[self.index] not in self.postAskTypes:
+                self.postAskTypes.append(self.needImportTypes[self.index])
+        else:
             if self.selectedPackage is not None:
-                self.importedPackages.append(self.selectedPackage)
                 if get_package_path(self.selectedPackage) in self.importedPackagesStat:
                     self.importedPackagesStat[get_package_path(self.selectedPackage)] += 1
                 else:
                     self.importedPackagesStat[get_package_path(self.selectedPackage)] = 1
-            self.index += 1
-            if self.index >= len(self.askTypes):
-                self.index = 0
-                self.run(edit, 6)
-            else:
-                self.run(edit, 4)
-        elif step == 6:
-            # import
-            add_action(
-                "javatar.command.operation.organize_imports.step6",
-                "Organize Imports [step=6] Import"
-            )
-            importCode = ""
+                self.importedPackages.append(self.selectedPackage)
+                self.importedTypes.append(self.needImportTypes[self.index])
+        self.index += 1
+        if self.index >= len(self.needImportTypes):
+            self.index = 0
+            self.run(edit, 3)
+        else:
+            self.run(edit, 1)
 
-            #clear old imports
-            packageRegions = self.view.find_by_selector(get_settings("package_meta_selector"))
-            if len(packageRegions) > 0:
-                importCode += self.view.substr(packageRegions[0]) + "\n\n"
-                self.view.replace(edit, packageRegions[0], "")
-            else:
-                importCode += "\n\n"
+    def organize_step_three(self, edit):
+        # add default imports
+        add_action(
+            "javatar.command.operation.organize_imports.step3",
+            "Organize Imports [step=3] Add default imports"
+        )
+        for packageImport in get_packages():
+            importOnce = False
+            if "packages" in packageImport:
+                for packageName in packageImport["packages"]:
+                    package = packageImport["packages"][packageName]
+                    for importType in get_all_types(package):
+                        if importType in self.askTypes:
+                            importOnce = True
+                            self.askTypes.remove(importType)
+                            if "default" in package and package["default"]:
+                                continue
+                            packageCode = packageName + "." + importType
+                            if packageCode not in self.importedPackages:
+                                self.importedPackages.append(packageCode)
+                            if packageCode in self.importedPackagesStat:
+                                self.importedPackagesStat[packageCode] += 1
+                            else:
+                                self.importedPackagesStat[packageCode] = 1
+                    if not importOnce and "always_import" in packageImport and packageImport["always_import"]:
+                        self.alwaysImportedPackages.append(packageName)
+        self.run(edit, 4)
 
+    def organize_step_four(self, edit):
+        # ask package
+        add_action(
+            "javatar.command.operation.organize_imports.step4",
+            "Organize Imports [step=4] Ask package"
+        )
+        self.askTypes += self.postAskTypes
+        if len(self.askTypes) > 0 and self.index < len(self.askTypes):
+            self.askPackage(-1, self.askTypes[self.index])
+        else:
+            self.run(edit, 6)
+
+    def organize_step_five(self, edit):
+        # ask package callback
+        add_action(
+            "javatar.command.operation.organize_imports.step5",
+            "Organize Imports [step=5] Ask package callback"
+        )
+        if self.selectedPackage is not None:
+            self.importedPackages.append(self.selectedPackage)
+            if get_package_path(self.selectedPackage) in self.importedPackagesStat:
+                self.importedPackagesStat[get_package_path(self.selectedPackage)] += 1
+            else:
+                self.importedPackagesStat[get_package_path(self.selectedPackage)] = 1
+        self.index += 1
+        if self.index >= len(self.askTypes):
+            self.index = 0
+            self.run(edit, 6)
+        else:
+            self.run(edit, 4)
+
+    def organize_step_six(self, edit):
+        # import
+        add_action(
+            "javatar.command.operation.organize_imports.step6",
+            "Organize Imports [step=6] Import"
+        )
+        importCode = ""
+
+        #clear old imports
+        packageRegions = self.view.find_by_selector(get_settings("package_meta_selector"))
+        if len(packageRegions) > 0:
+            importCode += self.view.substr(packageRegions[0]) + "\n\n"
+            self.view.replace(edit, packageRegions[0], "")
+        else:
+            importCode += "\n\n"
+
+        importsRegions = self.view.find_by_selector(get_settings("import_meta_selector"))
+        while len(importsRegions) > 0:
+            regionWithNewLine = sublime.Region(importsRegions[0].begin(), importsRegions[0].end() + 1)
+            while self.view.substr(regionWithNewLine)[-1] == "\n":
+                regionWithNewLine = sublime.Region(regionWithNewLine.begin(), regionWithNewLine.end() + 1)
+            regionWithNewLine = sublime.Region(regionWithNewLine.begin(), regionWithNewLine.end() - 1)
+            self.view.replace(edit, regionWithNewLine, "")
             importsRegions = self.view.find_by_selector(get_settings("import_meta_selector"))
-            while len(importsRegions) > 0:
-                regionWithNewLine = sublime.Region(importsRegions[0].begin(), importsRegions[0].end() + 1)
-                while self.view.substr(regionWithNewLine)[-1] == "\n":
-                    regionWithNewLine = sublime.Region(regionWithNewLine.begin(), regionWithNewLine.end() + 1)
-                regionWithNewLine = sublime.Region(regionWithNewLine.begin(), regionWithNewLine.end() - 1)
-                self.view.replace(edit, regionWithNewLine, "")
-                importsRegions = self.view.find_by_selector(get_settings("import_meta_selector"))
 
-            importedPackages = []
+        importedPackages = []
 
-            for alwaysImportPackage in self.alwaysImportedPackages:
-                for importPackage in self.importedPackages:
-                    if importPackage.startswith(alwaysImportPackage):
-                        self.importedPackages.remove(importPackage)
-
+        for alwaysImportPackage in self.alwaysImportedPackages:
             for importPackage in self.importedPackages:
-                if get_class_name_by_regex(importPackage) in self.useTypes:
-                    importedPackages.append(importPackage)
-            for importPackage in self.alwaysImportedPackages:
-                importedPackages.append(importPackage + ".*")
+                if importPackage.startswith(alwaysImportPackage):
+                    self.importedPackages.remove(importPackage)
 
-            if is_debug():
-                print(str(self.importedPackagesStat))
-            importedPackages.sort()
+        for importPackage in self.importedPackages:
+            if get_class_name_by_regex(importPackage) in self.useTypes:
+                importedPackages.append(importPackage)
+        for importPackage in self.alwaysImportedPackages:
+            importedPackages.append(importPackage + ".*")
 
-            for importPackage in importedPackages:
-                importCode += "import " + importPackage + ";\n"
+        if is_debug():
+            print(str(self.importedPackagesStat))
+        importedPackages.sort()
 
-            if importCode != "":
-                importCode += "\n"
-                #Remove whitespace at start of file
-                while re.search("\\s+$", self.view.substr(sublime.Region(0, 1))) is not None:
-                    self.view.replace(edit, sublime.Region(0, 1), "")
-                self.view.run_command("javatar_util", {"util_type": "insert", "text": importCode, "dest": "Organize Imports"})
-                if get_class_name() is None:
-                    className = "<Unknown>"
-                else:
-                    className = get_class_name()
-                sublime.set_timeout(lambda: show_status("Imports organized in class \"" + className + "\""), 500)
+        for importPackage in importedPackages:
+            importCode += "import " + importPackage + ";\n"
+
+        if importCode != "":
+            importCode += "\n"
+            #Remove whitespace at start of file
+            while re.search("\\s+$", self.view.substr(sublime.Region(0, 1))) is not None:
+                self.view.replace(edit, sublime.Region(0, 1), "")
+            self.view.run_command("javatar_util", {"util_type": "insert", "text": importCode, "dest": "Organize Imports"})
+            if get_class_name() is None:
+                className = "<Unknown>"
+            else:
+                className = get_class_name()
+            sublime.set_timeout(lambda: show_status("Imports organized in class \"" + className + "\""), 500)
+
+    def run(self, edit, step=0):
+        if step == 0:
+            self.organize_step_zero(edit)
+
+        elif step == 1:
+            self.organize_step_one(edit)
+
+        elif step == 2:
+            self.organize_step_two(edit)
+
+        elif step == 3:
+            self.organize_step_three(edit)
+
+        elif step == 4:
+            self.organize_step_four(edit)
+
+        elif step == 5:
+            self.organize_step_five(edit)
+
+        elif step == 6:
+            self.organize_step_six(edit)
 
     def selectClasses(self, index=None, classes=[]):
         if index is None:
