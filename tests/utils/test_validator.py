eeffde87806841e660e8bd19e975ffb2a53e55f7
Dominic May <jack.thatch@gmail.com>
2014-11-22 16:52:23 +0800

    Extend is_package tests

--- tests/utils/test_validator.py
+++ tests/utils/test_validator.py
@@ -1,10 +1,27 @@
+import sublime
 import unittest
+from unittest.mock import patch
+
+from javatar.utils.javatar_validator import is_package
 
 
 class TestValidator(unittest.TestCase):
-    def test_is_package(self):
-        from javatar.utils.javatar_validator import is_package
+    @patch('javatar.utils.javatar_utils.get_settings')
+    def test_is_package(self, get_settings):
+        get_settings.return_value = sublime.load_settings(
+            "Javatar.sublime-settings"
+        ).get('package_name_match')
 
         self.assertTrue(
             is_package('hello.world')
+        )
+
+    @patch('javatar.utils.javatar_utils.get_settings')
+    def test_is_package_special(self, get_settings):
+        get_settings.return_value = sublime.load_settings(
+            "Javatar.sublime-settings"
+        ).get('special_package_name_match')
+
+        self.assertTrue(
+            is_package('hello.world,')
         )
