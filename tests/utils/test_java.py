38ea195a70fad2198877f7ba90115750bac95ad6
Dominic May <jack.thatch@gmail.com>
2014-12-07 16:13:12 +0800

    Correct module paths

--- tests/utils/test_java.py
+++ tests/utils/test_java.py
@@ -4,13 +4,13 @@ import unittest
 from unittest.mock import patch
 import tempfile
 
-from javatar.utils.javatar_java import is_jdk_dir
+from Javatar.utils.javatar_java import is_jdk_dir
 
 create_file = lambda filename: open(filename, 'w').write('')
 
 
 class TestJava(unittest.TestCase):
-    @patch('javatar.utils.javatar_java.get_settings',
+    @patch('Javatar.utils.javatar_java.get_settings',
            return_value={'run': 'java', 'compile': 'javac'})
     def test_is_jdk_dir(self):
         direct = tempfile.mkdtemp()
